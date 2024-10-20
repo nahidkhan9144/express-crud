72c13d9c5f0426639b35e935ce54a7c8e247224c
nahid <nahidkahn9144@gmail.com>
2024-10-20 20:51:11 +0530

    commit......

--- app.js
+++ app.js
@@ -1,10 +1,17 @@
 const express = require('express');
-const app = express;
-const port = 3000;
+const app = express();
+const PORT = 3003;
 
-app.listen(port,(error)=>{
+app.listen(PORT,(error)=>{
     if(!error)
-        console.log('server is listining');
+        console.log('server is listining to port');
     else
         console.log('the error occured');
 })
+
+app.get('/',(req,res)=>{
+    console.log('success');
+    res.status(200);
+    res.send("Welcome to root URL of Server");
+})
+
