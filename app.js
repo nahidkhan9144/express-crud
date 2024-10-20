f5285e89e1f9535f1016d70074dc867a1e4b19bc
nahid <nahidkahn9144@gmail.com>
2024-10-20 21:08:42 +0530

    Router .

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 const express = require('express');
+
 const app = express();
 const PORT = 3003;
-
+const router = express.Router();
 app.listen(PORT,(error)=>{
     if(!error)
         console.log('server is listining to port');
@@ -13,5 +14,20 @@ app.get('/',(req,res)=>{
     console.log('success');
     res.status(200);
     res.send("Welcome to root URL of Server");
+});
+
+app.get('/router' , function (req,res,next){
+    console.log('Router Called');
+    res.status(200);
+    res.send("Welcome to router");
+    res.end();
+});
+
+app.use(router);
+
+app.get('/dbData',(req,res)=>{
+    console.log('success');
+    res.status(200);
+    res.send("DB");
 })
 
